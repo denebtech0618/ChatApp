388d70e05b84d37233aa7c3cff4557032c203497
Ivan Donyk <ivandonyk@gmail.com>
2017-01-08 15:10:28 -0500

    init commit

---
+++ includes/configure_static.php
@@ -0,0 +1,16 @@
+<?php
+
+define('JS_DIR', 'js/');
+define('CSS_DIR', 'css/');
+
+$arJsFilesNames = [
+    'jquery'=>'jquery-1.9.1.min.js',
+    'bootstrap'=>'bootstrap.js',
+    'chat'=>'chat.js',
+];
+
+$arCssFilesNames = [
+    'bootstrap'=>'bootstrap.css',
+    'style'=>'style.css'
+];
+
